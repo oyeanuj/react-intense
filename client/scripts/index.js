dc2e5dfcc4f23bd012febac0cd0952e39a289dc6
oyeAnuj <anujnijhawan@gmail.com>
2017-07-29 22:03:54 -0700

    Modifying ReactIntense to be just the viewer
    transform css not being set properly right now

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -6,39 +6,23 @@ import React from 'react'
 import { render } from 'react-dom'
 import ReactIntense from '../../lib/ReactIntense'
 
-const DemoImages = [{
+const DemoImages = [
+{
   caption: 'An annual month-long festival in Kyoto',
   classes: 'demo-image first',
   src: require("../img/horse.jpg"),
-  thumbnailSrc: require("../img/horse_thumb.jpg"),
   title: 'Gion Matsuri',
-},
-{ 
-  caption: 'Umbrellas are key!',
-  classes: 'demo-image second',
-  src: require("../img/rain.jpg"),
-  thumbnailSrc: require("../img/rain_thumb.jpg"),
-  title: 'Rainy rain',
-  vertical: true,
-}, {
-  caption: 'Ancient Buddhist temple on a cliff',
-  classes: 'demo-image third',
-  src: require("../img/temple.jpg"),
-  thumbnailSrc: require("../img/temple_thumb.jpg"),
-  title: 'Kiyomizu-dera',
 }]
 
 class IntenseDemos extends React.Component {
   _renderImages (images) {
     return images.map(
       ({ caption, classes, src, thumbnailSrc, title, vertical }) => (
-        <ReactIntense 
+        <ReactIntense
           caption={caption}
           classes={classes}
           key={title}
-          loader='uil-spin-css'
           src={src}
-          thumbnailSrc={thumbnailSrc}
           title={title}
           vertical={vertical}
         />
